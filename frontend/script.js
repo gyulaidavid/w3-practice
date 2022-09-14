72803a73b024c47f8219048b1cf8ba2c45534553
gyulaidavid <gyulaidavid@gmail.com>
2022-09-14 12:13:34 +0200

    js practice

---
+++ frontend/script.js
@@ -0,0 +1,38 @@
+// var myVariable --> SOHA NE HASZNÁLJ VAR-t!!!
+let myVariable = "value";
+// console.log(myVariable);
+
+myVariable = "new value";
+// console.log(myVariable);
+
+myVariable = 2;
+// console.log(myVariable);
+
+myVariable=true;
+// console.log(myVariable);
+
+let myVariable2 = "szöveg";
+// console.log(myVariable2);
+
+myVariable = myVariable2;
+// console.log(myVariable);
+
+let myVariable3 = myVariable+myVariable2;
+// console.log(myVariable3);
+
+// Függvény
+
+function sumVariables(variable1, variable2) {
+    console.log(variable1+variable2);
+}
+sumVariables(myVariable, myVariable2)
+sumVariables(8,"papa");
+sumVariables("word1" , "word2");
+
+function loadEvent (){
+console.log("loaded");
+let rootElement = document.getElementById("root");
+rootElement.insertAdjacentHTML("beforeend" , "Hello World");
+}
+
+window.addEventListener("click" , loadEvent);
