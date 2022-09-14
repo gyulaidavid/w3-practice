f6346d9f42a5bdbbcef25e551d3a1c16584fb8aa
gyulaidavid <gyulaidavid@gmail.com>
2022-09-14 12:18:13 +0200

    formated js

--- frontend/script.js
+++ frontend/script.js
@@ -8,7 +8,7 @@ myVariable = "new value";
 myVariable = 2;
 // console.log(myVariable);
 
-myVariable=true;
+myVariable = true;
 // console.log(myVariable);
 
 let myVariable2 = "szöveg";
@@ -17,22 +17,22 @@ let myVariable2 = "szöveg";
 myVariable = myVariable2;
 // console.log(myVariable);
 
-let myVariable3 = myVariable+myVariable2;
+let myVariable3 = myVariable + myVariable2;
 // console.log(myVariable3);
 
 // Függvény
 
 function sumVariables(variable1, variable2) {
-    console.log(variable1+variable2);
+  console.log(variable1 + variable2);
 }
-sumVariables(myVariable, myVariable2)
-sumVariables(8,"papa");
-sumVariables("word1" , "word2");
+sumVariables(myVariable, myVariable2);
+sumVariables(8, "papa");
+sumVariables("word1", "word2");
 
-function loadEvent (){
-console.log("loaded");
-let rootElement = document.getElementById("root");
-rootElement.insertAdjacentHTML("beforeend" , "Hello World");
+function loadEvent() {
+  console.log("loaded");
+  let rootElement = document.getElementById("root");
+  rootElement.insertAdjacentHTML("beforeend", "Hello World");
 }
 
-window.addEventListener("click" , loadEvent);
+window.addEventListener("click", loadEvent);
