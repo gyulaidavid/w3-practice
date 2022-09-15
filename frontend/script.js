b1e0becdd0efc592b9cbe1452e75a1d690b05806
gyulaidavid <gyulaidavid@gmail.com>
2022-09-15 16:39:21 +0200

    today practice

--- frontend/script.js
+++ frontend/script.js
@@ -1,38 +1,14 @@
-// var myVariable --> SOHA NE HASZNÁLJ VAR-t!!!
-let myVariable = "value";
-// console.log(myVariable);
-
-myVariable = "new value";
-// console.log(myVariable);
-
-myVariable = 2;
-// console.log(myVariable);
-
-myVariable = true;
-// console.log(myVariable);
-
-let myVariable2 = "szöveg";
-// console.log(myVariable2);
-
-myVariable = myVariable2;
-// console.log(myVariable);
-
-let myVariable3 = myVariable + myVariable2;
-// console.log(myVariable3);
-
-// Függvény
-
-function sumVariables(variable1, variable2) {
-  console.log(variable1 + variable2);
-}
-sumVariables(myVariable, myVariable2);
-sumVariables(8, "papa");
-sumVariables("word1", "word2");
 
 function loadEvent() {
-  console.log("loaded");
-  let rootElement = document.getElementById("root");
-  rootElement.insertAdjacentHTML("beforeend", "Hello World");
+const newCatButton = document.querySelector(".add-cat")  ;
+newCatButton.addEventListener("click" , function() {
+const catContainer = document.querySelector(".cat-conatiner");
+const now = new Date ().getTime();
+const newCat = `<img src="https://cataas.com/cat?random=${now}" alt="Új cica" />`;
+catContainer.insertAdjacentHTML("beforeend", newCat);
+})
 }
 
-window.addEventListener("click", loadEvent);
+
+window.addEventListener("load", loadEvent);
+    
